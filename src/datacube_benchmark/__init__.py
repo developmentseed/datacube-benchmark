d8abbafc53d43ec3ccf52d0494262f2e219d5945
Max Jones <14077947+maxrjones@users.noreply.github.com>
2026-05-31 21:54:24 -0400

    Remove entrypoint

--- src/datacube_benchmark/__init__.py
+++ src/datacube_benchmark/__init__.py
@@ -4,12 +4,9 @@ from .create import (
     create_or_open_zarr_array,
     create_or_open_zarr_store,
 )
-from .create import Quantity
 from .config import Config
 from .query import benchmark_zarr_array, benchmark_access_patterns
 from .open import benchmark_dataset_open
-
-import numpy as np
 
 try:
     from ._version import __version__
@@ -27,12 +24,3 @@ __all__ = [
     "benchmark_access_patterns",
     "benchmark_dataset_open",
 ]
-
-
-def main() -> None:
-    da = create_empty_dataarray()
-    array_size = Quantity(da.nbytes, "bytes")
-    chunk_size = Quantity(np.prod(da.data.chunksize) * da.dtype.itemsize, "bytes")
-    print(da)
-    print(f"Array size: {array_size.to('GB')}")
-    print(f"Chunk size: {chunk_size.to('MB')}")
